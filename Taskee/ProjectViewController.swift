f6010f2b4c0d8d87f3b9098073e3387b1bd2928b
bsimps01 <b.albertsimpson@gmail.com>
2020-09-26 15:23:23 -0700

    added project starter

---
+++ Taskee/ProjectViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Taskee
+//
+//  Created by Benjamin Simpson on 9/24/20.
+//
+
+import UIKit
+import CoreData
+
+class ProjectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
